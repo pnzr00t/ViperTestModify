03c4ef19d5e87d1004609d01385e717548eaf4eb
pnzr00t <pnz.r00t@gmail.com>
2019-11-07 14:19:09 +0300

    Initial Commit

---
+++ ViperTestModify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperTestModify
+//
+//  Created by 	Oleg2 on 07.11.2019.
+//  Copyright © 2019 Oleg Pustoshkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
